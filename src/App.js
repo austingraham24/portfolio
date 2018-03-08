fdcfa40d9e18f66c1e741a703e3c0301dd3fd2ee
Austin Graham <austingraham24@gmail.com>
2018-03-08 09:23:22 -0600

    testing react-bootstrap stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Alert } from 'react-bootstrap';
 
 class App extends Component {
   render() {
@@ -13,6 +14,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Alert bsStyle="warning">
+          <strong>Holy guacamole!</strong> Best check yo self, you're not looking too
+          good.
+        </Alert>;
       </div>
     );
   }
